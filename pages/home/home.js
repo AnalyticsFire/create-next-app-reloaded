324be23ab3c2a8474bbb229b328541f751989805
Damian Aruj <aruj.damian@gmail.com>
2018-05-09 18:41:13 -0300

    implemented i18n

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,9 +1,15 @@
 import Layout from '../../components/layout';
+import { func } from 'prop-types';
+import { withI18next } from '../../lib/withI18next';
 
-const Home = () => (
+const Home = ({ t }) => (
   <Layout title="Home" className="home-page">
-    <h1>Home</h1>
+    <h1>{t('title')}</h1>
   </Layout>
 );
 
-export default Home;
+Home.propTypes = {
+  t: func,
+};
+
+export default withI18next(['home'])(Home);
