c554344d1d9dcc65978fa75417a094bbbd7d490a
Damian Aruj <aruj.damian@gmail.com>
2018-05-12 13:57:09 -0300

    updated i18n with a config variable

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Layout from '@root/components/layout';
 import { func } from 'prop-types';
+import config from 'config';
 import { withI18next } from '@root/lib/withI18next';
 
 export const Home = ({ t }) => (
   <Layout title={t('home:pageTitle')} className="home-page">
     <div className="container pt-4 pb-4">
-      <h1 className="title text-center pb-3">{t('home:title')}</h1>
+      <h1 className="title text-center pb-3">
+        {t('home:title', { env: config.get('env').toUpperCase() })}
+      </h1>
       <img src="static/images/the-team.jpg" className="w-100" />
     </div>
   </Layout>
